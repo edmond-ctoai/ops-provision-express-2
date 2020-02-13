3b810cdacea94c956557840231bb5f41ebc6899e
Edmond Lee <edmond+remax@hackcapital.com>
2020-02-12 20:07:09 -0800

    update db starter

--- db.js
+++ db.js
@@ -1,23 +1,11 @@
-// const pg = require('pg');
-// const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
-
-// const client = new pg.Client(connectionString);
-// client.connect();
-// const query = client.query(
-//   'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-// query.on('end', () => {
-//   client.end();
-// });
-
 const Pool = require('pg').Pool
 const pool = new Pool({
-  database: 'ebdb',
+  database: 'postgres',
   host: process.env.RDS_HOSTNAME,
   user: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
   port: process.env.RDS_PORT
 })
-console.log("hola")
 
 pool.query(`CREATE TABLE content (ID SERIAL PRIMARY KEY, data text);`, (err, res) => {
   console.log(err, res);
